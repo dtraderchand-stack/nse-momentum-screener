4c1229ae49c483b716bfb899cc16660a2194bbd1
dtraderchand-stack <dtraderchand@gmail.com>
2026-07-29 11:32:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+document.getElementById("scan").onclick = function () {
+
+    document.getElementById("status").innerHTML =
+        "Status : Scanner Started...";
+
+    setTimeout(function(){
+
+        document.getElementById("status").innerHTML =
+        "Found Stocks : Waiting for Fyers API connection";
+
+    },2000);
+
+};
