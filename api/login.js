0e239c30f33770e48a8184210283f56cbf196977
dtraderchand-stack <dtraderchand@gmail.com>
2026-07-30 10:00:43 +0530

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,14 @@
 
+export default function handler(req, res) {
+  const APP_ID = "1PMA5T4004-200";
+  const REDIRECT_URI = "https://nse-momentum-screener.vercel.app/";
+
+  const url =
+    `https://trade.fyers.in/api-login/redirect-uri/index.html?client_id=${APP_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&state=123`;
+
+  res.writeHead(302, {
+    Location: url,
+  });
+
+  res.end();
+}
