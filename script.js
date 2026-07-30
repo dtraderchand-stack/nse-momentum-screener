6f4563ff701da1c170c8782c7be3a345e64e01c1
dtraderchand-stack <dtraderchand@gmail.com>
2026-07-30 09:53:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const APP_ID = "1PMA5T4004-200";
+const REDIRECT_URI = "https://nse-momentum-screener.vercel.app/";
+
 document.getElementById("scanBtn").onclick = () => {
-    document.getElementById("result").innerHTML = "Scanner Started...";
+    const authUrl =
+        `https://api-t1.fyers.in/api/v3/generate-authcode?client_id=${APP_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&state=123`;
+
+    window.location.href = authUrl;
 };
