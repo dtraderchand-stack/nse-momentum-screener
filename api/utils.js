bf2d69c3a89a42a4eb18fad4703ae675f475539e
dtraderchand-stack <dtraderchand@gmail.com>
2026-07-31 12:17:45 +0530

    Update utils.js

--- api/utils.js
+++ api/utils.js
@@ -1,21 +1,35 @@
 export function chunkArray(array, size) {
-  const chunks = [];
+  const result = [];
 
   for (let i = 0; i < array.length; i += size) {
-    chunks.push(array.slice(i, i + size));
+    result.push(array.slice(i, i + size));
   }
 
-  return chunks;
+  return result;
 }
 
 export function formatSymbol(symbol) {
   return `NSE:${symbol}-EQ`;
 }
 
-export function calculateCandleBody(open, close) {
+export function sortByChange(quotes) {
+  return quotes.sort((a, b) => b.change - a.change);
+}
+
+export function topGainers(quotes, count) {
+  return quotes.slice(0, count);
+}
+
+export function filterByChange(quotes, min, max) {
+  return quotes.filter(
+    stock => stock.change >= min && stock.change < max
+  );
+}
+
+export function candleBody(open, close) {
   return Math.abs(((close - open) / open) * 100);
 }
 
-export function calculateTradedValue(volume, close) {
+export function tradedValue(volume, close) {
   return volume * close;
 }
