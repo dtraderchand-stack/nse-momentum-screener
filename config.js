62ca618bbf47f3ca66f97c3a808d4acdab8d941b
dtraderchand-stack <dtraderchand@gmail.com>
2026-07-31 12:16:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,19 @@
-const CONFIG = {
-    appId: "1PMA5T4004-200",
-    redirectUrl: "https://nse-momentum-screener.vercel.app/"
-};
+export const APP_ID = "1PMA5T4004-200";
+
+export const BATCH_SIZE = 50;
+
+export const TOP_GAINERS = 100;
+
+export const MIN_CHANGE = 4;
+
+export const MAX_CHANGE = 12;
+
+export const MIN_CANDLE_BODY = 1;
+
+export const MIN_TRADED_VALUE = 100000000; // ₹10 Crore
+
+export const CACHE_TIME = 10000;
+
+export const QUOTE_URL = "https://api-t1.fyers.in/data/quotes";
+
+export const HISTORY_URL = "https://api-t1.fyers.in/data/history";
