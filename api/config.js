24a843e7658c28f9f9de361f4b71c4c3ece67e15
dtraderchand-stack <dtraderchand@gmail.com>
2026-07-31 11:31:53 +0530

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,1 +1,13 @@
+export const APP_ID = "1PMA5T4004-200";
 
+export const BATCH_SIZE = 50;
+
+export const TOP_GAINERS = 100;
+
+export const MIN_CHANGE = 4;
+
+export const MAX_CHANGE = 12;
+
+export const MIN_CANDLE_VALUE = 1000000000; // ₹10 Crore
+
+export const MIN_CANDLE_BODY = 1; // 1%
